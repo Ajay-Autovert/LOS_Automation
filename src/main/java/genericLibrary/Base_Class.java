565778e4f2a97281b0de08ba253b8b027171fc46
Ajay1 <ajay1@autovert.in>
2025-09-22 16:07:21 +0530

    Added chome option to make browser headless

--- src/main/java/genericLibrary/Base_Class.java
+++ src/main/java/genericLibrary/Base_Class.java
@@ -36,9 +36,9 @@ public class Base_Class implements FrameworkConstants {
 			WebDriverManager.chromedriver().clearDriverCache().setup();
 			
 			ChromeOptions options = new ChromeOptions();
-//			options.addArguments("--headless=new");
-//			options.addArguments("--no-sandbox");
-//			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--headless=new");
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
 			options.addArguments("--disable-save-password-bubble");
 			options.addArguments("--disable-notifications");
 			options.addArguments("--guest"); // Open in Guest mode
